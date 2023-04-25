77dc773144175f6a9c59cc8b4db7c83aae78345f
Niccolo <niccolo.maccarini@studenti.unipd.it>
2023-04-25 20:02:42 +0200

    Initial commit

---
+++ wittgenstein_change_encod/__init__.py
@@ -0,0 +1,10 @@
+# Author: Ilan Moscovitz <ilan.moscovitz@gmail.com>
+# License: MIT
+# This version contains a few improvement written by myself - Niccolò
+
+from .irep import IREP
+from .ripper import RIPPER
+import pandas as pd
+
+__version__ = "0.3.2"
+__author__ = "Ilan Moscovitz"
